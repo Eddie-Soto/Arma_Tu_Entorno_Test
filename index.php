e9ca28b295995b9973fc92e6161d85605d28da0e
Eddie-Soto <esoto@nikkenlatam.com>
2021-11-29 10:35:05 -0600

    Update index.php

--- index.php
+++ index.php
@@ -448,6 +448,17 @@ include('assets/include/sessions.php');
 
 	<script> $.idle(600, function() {window.location.href = "https://mitiendanikken.com/"; }); </script><!-- CERRAR SESION INACTIVO -->
 
+<script type="text/javascript">
+	   //Cuando la página esté cargada completamente
+  $(document).ready(function(){
+    //Cada 10 segundos (10000 milisegundos) se ejecutará la función refrescar
+    setTimeout(refrescar, 60000);
+  });
+  function refrescar(){
+    //Actualiza la página
+    location.reload();
+  }
+</script>
 	<!-- Librerias adicionales -->
 
 </div>
