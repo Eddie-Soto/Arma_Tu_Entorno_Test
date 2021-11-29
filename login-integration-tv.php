97a7d789d48ab718a7cea8d11dc51e92d521c179
Eddie-Soto <esoto@nikkenlatam.com>
2021-11-29 11:43:31 -0600

    Update login-integration-tv.php

--- login-integration-tv.php
+++ login-integration-tv.php
@@ -45,9 +45,9 @@ if($type == "TVNCI")
 	/*$code_abi = base64_decode($_GET["sap_code"]);
 	$email_abi = base64_decode($_GET["email"]);
 	$country_abi = $_GET["country"];*/
-	$code_abi='22718303';
-	$email_abi='laule.yessica@gmail.com';
-	$country_abi='3';
+	$code_abi='18480803';
+	$email_abi='mtsiman@gmail.com';
+	$country_abi='7';
 	$type_client = "ab";
 
 	/*consultar asesor*/
