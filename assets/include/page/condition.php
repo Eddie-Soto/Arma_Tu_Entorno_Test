0923b27c57b07d19a222a1e49d2c41f95e5e72d6
Maikol04 <mgarciar@nikkenlatam.com>
2022-12-14 11:08:07 -0600

    Update condition.php

--- assets/include/page/condition.php
+++ assets/include/page/condition.php
@@ -525,7 +525,8 @@ if ($environment == "STEPS") {
 					<p class="help-block padding-bottom"><small><i class="color-green fa <?php if (Valite_personal_environment_one() == 0) {
 																								echo "fa-circle-o";
 																							} else {
-																								echo "fa-check-circle-o";
+																								//echo "fa-check-circle-o";
+																								echo Valite_personal_environment_one();
 																							} ?> fa-2x" aria-hidden="true"></i>&nbsp;&nbsp;<strong><span class="color-green">1°</span> Escoge al menos 5 unidades de cualquier producto de las marcas abajo relacionadas. Puedes combinar o puede ser de la misma referencia.</strong></small><br /><span style="font-size: 15px;"><small><u>Ten en cuenta que los productos adicionales que no pertenezcan al Entorno Personal <strong>no aplican para el descuento especial</strong>, a pesar de que ya se haya cumplido la condición o regla del entorno.</u></small></span></p>
 				<?php
 				}
