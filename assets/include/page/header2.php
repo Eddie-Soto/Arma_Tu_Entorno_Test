4ce01fac528d990c124269b474a1a8fd50fa3d7b
Maikol04 <mgarciar@nikkenlatam.com>
2023-04-04 16:19:43 -0600

    Update header2.php

--- assets/include/page/header2.php
+++ assets/include/page/header2.php
@@ -3,8 +3,8 @@
 include('../sessions.php');
 
 ?>
-<div class="navbar navbar-inverse center" style="margin-bottom: 0;" role="navigation">
-  	<div class="container center title-ate" style="background-color: #0039c0">
+<div class="navbar navbar-inverse center" style="margin-bottom: 0;background-color: #0039c0;" role="navigation">
+  	<div class="container center title-ate">
         <h1 style="color:white">ACCESORIOS</h1>
   	</div>
 </div>
