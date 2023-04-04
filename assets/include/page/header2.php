978e7a06b77eaf36a16144dfd7d8f53a14fc0ca0
Maikol04 <mgarciar@nikkenlatam.com>
2023-04-04 15:58:03 -0600

    tst

--- assets/include/page/header2.php
+++ assets/include/page/header2.php
@@ -4,7 +4,7 @@ include('../sessions.php');
 
 ?>
 <div class="navbar navbar-inverse center" style="margin-bottom: 0;" role="navigation">
-  	<div class="container center">
+  	<div class="container center title-ate">
         <h1>Accesorios</h1>
   	</div>
 </div>
