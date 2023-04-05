fe51d6ccee83bde42264b06fc69fa20c5e722da3
Maikol04 <mgarciar@nikkenlatam.com>
2023-04-05 16:08:30 -0600

    Update header2.php

--- assets/include/page/header2.php
+++ assets/include/page/header2.php
@@ -5,7 +5,8 @@ include('../sessions.php');
 ?>
 <div class="navbar navbar-inverse center" style="margin-bottom: 0;background-color: #0039c0;" role="navigation">
   	<div class="container center title-ate">
-        <h1 style="color:white">ACCESORIOS</h1>
+        <h1 style="color:white">ACCESORIOS </h1>
+        <h3><?php echo $_SESSION['environment'] ?></h3>
   	</div>
 </div>
 <hr class="line">
